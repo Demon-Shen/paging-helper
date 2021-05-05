19b695d92b2f84a1d74e6717e8999faad9466b02
SweetCode <shenwt@cnlemon.net>
2021-05-05 20:11:23 +0800

    misc: 版本号修改

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
  * @Author: sweet
  * @Date: 2020-12-09 13:46:35
  * @LastEditors: sweet
- * @LastEditTime: 2021-05-05 19:21:08
+ * @LastEditTime: 2021-05-05 20:10:33
  * @Description: file content
  */
 import PagingHelperMobile from "./build/PagingHelperMobile";
 import PagingHelperPC from "./build/PagingHelperPC";
-import usePageingHelperPc from "./build/PagingHelperPC";
+import usePageingHelperPc from "./build/usePageingHelperPc";
 
 export { PagingHelperMobile, PagingHelperPC, usePageingHelperPc };
